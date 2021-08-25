7a282d35046a396332964c852f67b21138539381
Christian <christianschiavini@outlook.com>
2021-08-25 16:56:48 -0300

    adiciona gitignore e soma.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print('Meu primeiro arquivo indo para o git')
 
 print('Meu segundo arquivo indo para o git')
+
+from soma import Soma
+
+a = 3
+b = 4
+soma = Soma()
+resultado = soma.soma(a, b)
+
+print('resultado da soma = ' + str(resultado))
